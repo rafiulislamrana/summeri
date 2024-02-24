93121e859fa7203589ae41bf8727209d7530fef5
rafiulislamrana <irafiul802@gmail.com>
2024-02-25 00:08:21 +0600

    redux store created

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
 import {copy, linkIcon, loader, tick} from "../assets";
 const Demo = () => {
+    const [article, setArticle] = useState({
+        url: "",
+        summery: "",
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+       
+        
+    }
 
     return (
-        const [article, setArticle] = useState()
         <section className="w-full max-w-xl  mt-16">
             {/* search */}
             <div className="flex flex-col w-full gap-2">
-                <form className="relative flex justify-center items-center" onSubmit={() => {}}>
+                <form className="relative flex justify-center items-center" onSubmit={handleSubmit}>
                     <img src={linkIcon} className="absolute left-0 my-2 ml-3 w-5" alt="link_icon" />
-                    <input type="url" placeholder="Enter an URL" value="" onChange={() => {}} required className="url_input peer" />
-                    <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">=></button>
+                    <input type="url" placeholder="Enter an URL" value={article.url} onChange={(e) => setArticle({ ...article, url: e.target.value})} required className="url_input peer" />
+                    <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">Go</button>
                 </form>
 
                 {/* URL Browser History */}
