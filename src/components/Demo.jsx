8a302e48a68f6e5a8b75d5609fbba17345b5672e
rafiulislamrana <irafiul802@gmail.com>
2024-02-25 01:39:33 +0600

    summary displayed at the page

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -11,6 +11,7 @@ const Demo = () => {
     });
 
     const [allArticles, setAllArticles] = useState([]);
+    const [copied, setCopied] = useState("");
 
     useEffect(() => {
         const articlesFromLocalStorage = JSON.parse(localStorage.getItem("articles"))
@@ -37,6 +38,12 @@ const Demo = () => {
         
     }
 
+    const handleCopy = (copy) => {
+        setCopied(copy);
+        navigator.clipboard.writeText(copy);
+        setTimeout(() => setCopied(false), 3000)
+    }
+
     return (
         <section className="w-full max-w-xl  mt-16">
             {/* search */}
@@ -52,8 +59,8 @@ const Demo = () => {
                     {
                         allArticles.map((item,idx) => (
                             <div key={idx} onClick={() => setArticle(item)} className="link_card">
-                                <div className="copy_btn">
-                                    <img src={copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain" />
+                                <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                                    <img src={copied === item.url ? tick : copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain" />
                                 </div>
                                 <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{item.url}</p>
                             </div>
@@ -64,6 +71,24 @@ const Demo = () => {
             </div>
 
             {/* Display Summery Result */}
+            <div className="my-10 max-w-full flex justify-center items-center">
+                    {isFetching ? (
+                        <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+                    ): error ? (
+                        <p className="font-inter font-bold text-black text-center">Ops! Service is busy <br/>
+                        <span className="font-satoshi font-normal text-gray-700">{
+                            error?.data?.error}</span></p>
+                    ): (
+                        article.summery && (
+                            <div className="flex flex-col gap-3">
+                                <h2 className="font-satoshi font-bold text-gray-600 text-xl">Article <span className="blue_gradient">Summary</span></h2>
+                                <div className="summary_box">
+                                    <p className="font-inter font-medium text-sm text-gray-700">{article.summery}</p>
+                                </div>
+                            </div>
+                        )
+                    )}
+            </div>
         </section>
     );
 };
